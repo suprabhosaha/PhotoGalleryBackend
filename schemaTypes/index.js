683fcdf17220543d23683c99c76efcffb2fe8b39
suprabhosaha <suprabho04@gmail.com>
2024-08-18 20:02:31 +0530

    Initialising backend and setting up schemas

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,13 @@
-export const schemaTypes = []
+import comment from "./comment";
+import gallery from "./gallery";
+import postedBy from "./postedBy";
+import save from "./save";
+import user from "./user";
+
+export const schemaTypes = [
+    user,
+    comment,
+    gallery,
+    postedBy,
+    save
+]
---
+++ schemaTypes/user.js
@@ -0,0 +1,18 @@
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'useName',
+            title: 'Username',
+            type: 'string',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string',
+        }
+    
+    ]
+}
